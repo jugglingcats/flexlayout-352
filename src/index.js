efb117e02ae44be574536405199d3a4d59629197
jugglingcats <github@akirkpatrick.com>
2022-12-01 16:55:18 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {StrictMode, Suspense} from "react";
+import {createRoot} from "react-dom/client";
+import {Layout, Model} from "flexlayout-react";
+import {Canvas} from "@react-three/fiber";
+import {suspend} from "suspend-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "/node_modules/flexlayout-react/style/light.css";
+
+/**
+ * This component uses suspend from the suspend-react package. This is used internally by @react-three/fiber and @react-three/drei so hard to avoid.
+ */
+const TroublesomeComponent = () => {
+    suspend(() => new Promise(resolve => setTimeout(() => resolve(true), 300)), [])
+
+    return <mesh rotation={[Math.PI / 8, Math.PI / 8, Math.PI / 8]}>
+        <boxGeometry attach="geometry" args={[1, 1, 1]}/>
+        <meshBasicMaterial attach="material" color="red"/>
+    </mesh>
+}
+
+const MODEL = {
+    global: {},
+    layout: {
+        type: "row", id: "root", children: [{type: "tabset", children: [{type: "tab", name: "Tab 1", id: "t1"}]}]
+    }
+};
+
+/**
+ * Version of app that uses flexlayout-react
+ */
+export default function App() {
+    const model = Model.fromJson(MODEL);
+
+    const factory = () => {
+        return (
+            <Canvas>
+                <TroublesomeComponent/>
+            </Canvas>
+
+            // Issue can be worked around by adding a nested Suspense!
+            // <Suspense>
+            //     <Canvas>
+            //         <TroublesomeComponent/>
+            //     </Canvas>
+            // </Suspense>
+        );
+    };
+
+    return <Layout model={model} factory={factory}/>;
+}
+
+/**
+ * Version of app that doesn't use flexlayout-react
+ * (doesn't exhibit the issue with or without second Suspense)
+ */
+// export default function App() {
+//     return <Canvas>
+//         <TroublesomeComponent/>
+//     </Canvas>
+// }
+
+const root = createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <StrictMode>
+        <div style={{width: "90vw", height: "90vh"}}>
+            {/*
+            We have an outer Suspense in our app to handle code splitting (React.lazy and dynamic imports)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+            If you remove this outer Suspense, error does not occur with or without nested Suspense
+            */}
+            <Suspense>
+                <App/>
+            </Suspense>
+        </div>
+    </StrictMode>
+);
